eab9facfe7f134e106b8647c500cccc15670a4d5
Gild-Shogi <ok2336.coffee@gmail.com>
2022-11-18 16:11:34 +0900

    被りの除去

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from typing import List, Union
+from typing import List, Union, Set
 import json
 
 app = FastAPI()
@@ -15,13 +15,15 @@ async def root(request:Request):
     return templates.TemplateResponse("main.html", {"request":request})
 
 @app.get("/result/", response_class=HTMLResponse)
-async def result(request:Request, ing:Union[List[str], None] = Query(default=None)):
+async def result(request:Request, ing:Union[Set[str], None] = Query(default=None)):
     json_open = open('fixture/recipe.json', mode = 'r', encoding = 'UTF-8')
     json_load = json.load(json_open)
     recipe = []
+    urls = []
     for i in json_load:
         for item in ing:
-            if item in i["tags"]:
+            if item in i["tags"] and not(i["url"] in urls):
                 recipe.append(i)
-    #recipe = dict(set(recipe))
+                urls.append(i["url"])
+    
     return templates.TemplateResponse("result.html", {"request":request, "ing":ing, "recipe":recipe})
