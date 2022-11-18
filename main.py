c4501fe336d307a156cc7f8f28e863a567038cc9
Gild-Shogi <ok2336.coffee@gmail.com>
2022-11-18 16:06:07 +0900

    複数キーワードでの検索

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import List, Union
 import json
 
 app = FastAPI()
@@ -14,11 +15,13 @@ async def root(request:Request):
     return templates.TemplateResponse("main.html", {"request":request})
 
 @app.get("/result/", response_class=HTMLResponse)
-async def result(request:Request, ing:str):
+async def result(request:Request, ing:Union[List[str], None] = Query(default=None)):
     json_open = open('fixture/recipe.json', mode = 'r', encoding = 'UTF-8')
     json_load = json.load(json_open)
     recipe = []
     for i in json_load:
-        if ing in i["tags"]:
-            recipe.append(i)
+        for item in ing:
+            if item in i["tags"]:
+                recipe.append(i)
+    #recipe = dict(set(recipe))
     return templates.TemplateResponse("result.html", {"request":request, "ing":ing, "recipe":recipe})
