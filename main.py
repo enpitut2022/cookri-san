efd3d937b4451ff113425ab731ea80a6064f3a8b
Gild-Shogi <ok2336.coffee@gmail.com>
2022-12-09 17:05:17 +0900

    notのマルチ検索とvalueの設定

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ async def root(request:Request):
     return templates.TemplateResponse("main.html", {"request":request})
 
 @app.get("/result/", response_class=HTMLResponse)
-async def result(request:Request, ing:Set[str] = Query(default=None), search_not:Set[str] = Query(default=None)):
+async def result(request:Request, ing:Set[str] = Query(default=set()), search_not:Set[str] = Query(default=None)):
     json_open = open('fixture/recipe2.json', mode = 'r', encoding = 'UTF-8')
     json_load = json.load(json_open)
     recipe = []
     #urls = []
+    # ing = ["鶏もも肉", "ピーマン"]　⇚　main.htmlで2つ以上入力したとき
+    # ing = ["鶏もも肉 ピーマン"]　⇚ result.htmlで2つ入力したとき
+    if len(ing) == 1:
+        ing = set(list(ing)[0].split())
     for i in json_load:
         if search_not is not None:
-            if set(i["tags"]) & ing == ing and set(i["tags"]) & search_not == set():
+            query_not = list(search_not)[0].split()
+            query_not = set(query_not)
+            if set(i["tags"]) & ing == ing and set(i["tags"]) & query_not == set():
                 recipe.append(i)
         else:
+            search_not = set()
             if set(i["tags"]) & ing == ing:
                 recipe.append(i)
-    
-    return templates.TemplateResponse("result.html", {"request":request, "ing":ing, "recipe":recipe})
+    ing_s = " ".join(list(ing))
+    not_s = " ".join(list(search_not))
+    return templates.TemplateResponse("result.html", {"request":request, "ing":ing, "recipe":recipe, "ing_s":ing_s, "not_s": not_s})
